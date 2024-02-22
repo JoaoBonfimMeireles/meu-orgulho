7c47ef59b196ac63f2fee7941887ecd6a0aee935
JoaoBonfimMeireles <jpwmeireles44@gmail.com>
2024-02-22 06:36:16 -0300

    forma da rocket

--- script.js
+++ script.js
@@ -1,30 +1,50 @@
-console.log('teste')
+// console.log('teste')
 
-let math = Math.round(Math.random() * 10)
-let type = Number(prompt('fale o número? '))
-let types = [type]
+// let math = Math.round(Math.random() * 10)
+// let type = Number(prompt('fale o número? '))
+// let types = [type]
 
-console.log("1", type)
+// console.log("1", type)
 
-function conferir() {
-    if(math === type) {
-        alert('parabens infeliz!!')
-        return true
+// function conferir() {
+//     if(math === type) {
+//         alert('parabens infeliz!!')
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+// while(!conferir()){
+//     type = Number(prompt('fale outro numero: '))
+//     types.push(type)
+
+//     console.log('2', types)
+// }
+
+// let quantidade = types.length
+// let text = (quantidade > 1) ? "tentativas" : "uma tentativa"; 
+
+// alert(`parabens fulano, vc acertou em ${quantidade} ${text}`)
+
+// console.log('gh', quantidade)
+
+let result = prompt('coloquei o numero:')
+let randomNumber = Math.round(Math.random() * 10)
+
+let xAttempts = 0;
+
+while (Number(result) != randomNumber) {
+    result = prompt('coloque outro numero: ')
+    xAttempts++
+}
+
+function quantidade() {
+    if (xAttempts > 1) {
+        return 'vezes'
     } else {
-        return false
+        return 'tentativa'
     }
 }
 
-while(!conferir()){
-    type = Number(prompt('fale outro numero: '))
-    types.push(type)
-    
-    console.log('2', types)
-}
-
-let quantidade = types.length
-let text = (quantidade > 1) ? "tentativas" : "uma tentativa"; 
-
-alert(`parabens fulano, vc acertou em ${quantidade} ${text}`)
-
-console.log('gh', quantidade)
+alert(`boa em casa, foi em ${xAttempts} ${quantidade()}`)
